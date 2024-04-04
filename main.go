a1b11e0d6ef0999308c5c0c3402349f0fd24265a
mauricioumelo <mauricioumelo@gmail.com>
2024-04-04 12:02:46 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/mauricioumelo/gopportunities/router"
+import (
+	"github.com/mauricioumelo/gopportunities/config"
+	"github.com/mauricioumelo/gopportunities/router"
+)
+
+var logger config.Logger
 
 func main() {
+	logger = *config.GetLogger("MAIN")
+	//Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	//Initialize Router
 	router.Initialize()
 }
