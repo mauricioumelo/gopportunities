34af9d3bc9e955b2a2d822fb282c7a082a05ffc8
mauricioumelo <mauricioumelo@gmail.com>
2024-04-09 10:35:30 -0300

    feat: add method show opening

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -1,13 +1,26 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mauricioumelo/gopportunities/schemas"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Show message opening!",
-	})
+	or := schemas.OpeningRequest{}
+
+	if err := ctx.ShouldBindUri(&or); err != nil {
+		SendError(ctx, http.StatusBadRequest, errValueIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, or.ID).Error; err != nil {
+		SendError(ctx, http.StatusNotFound, fmt.Errorf("opening with id: %s not found", or.ID).Error())
+		return
+	}
+	SendSuccess(ctx, http.StatusOK, "Success show opening", opening)
 }
